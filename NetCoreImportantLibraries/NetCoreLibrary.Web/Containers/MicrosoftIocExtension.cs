cd7f40aab5b2910b49bbe4fd4386ab2298ab91f0
Cihat Solak <cihatsolak@hotmail.com>
2021-04-11 18:15:26 +0300

    Basic response model for api controller

--- NetCoreImportantLibraries/NetCoreLibrary.Web/Containers/MicrosoftIocExtension.cs
+++ NetCoreImportantLibraries/NetCoreLibrary.Web/Containers/MicrosoftIocExtension.cs
@@ -1,8 +1,11 @@
 ﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NetCoreLibrary.Data;
+using NetCoreLibrary.Web.Models;
+using System.Linq;
 
 namespace NetCoreLibrary.Web.Containers
 {
@@ -23,6 +26,23 @@ namespace NetCoreLibrary.Web.Containers
                 //Startup assemblyine bulunan IValidator interfacesinden miras almış classları servis olarak otomatik ekle
                 //Proje içerisindeki tüm IValidator olan classları birer servis olarak kaydedecektir.
                 options.RegisterValidatorsFromAssemblyContaining<Startup>();
+            });
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                // options.SuppressModelStateInvalidFilter = true; -> ModelState Invalid oldugunda da gelen request'i kabul et 
+
+                //ModelState Invalid olduğunda döneceğim responsemodel
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errorResponseModel = new ErrorResponseModel
+                    {
+                        Status = 400,
+                        Errors = context.ModelState.Values.Where(p => p.Errors.Count > 0).SelectMany(p => p.Errors).Select(p => p.ErrorMessage).ToList()
+                    };
+
+                    return new BadRequestObjectResult(errorResponseModel);
+                };
             });
         }
     }
---
+++ NetCoreImportantLibraries/NetCoreLibrary.Web/Controllers/WebApi/CustomersApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLibrary.Core.Domain;
+
+namespace NetCoreLibrary.Web.Controllers.WebApi
+{
+    [ApiController, Route("api/[controller]/[action]")]
+    public class CustomersApiController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add(Customer customer)
+        {
+            return Ok();
+        }
+    }
+}
