a3ba30b6ef3fa5f6520e219c0e76e537364c698c
Cihat Solak <cihatsolak@hotmail.com>
2021-04-11 12:47:33 +0300

    Initial Create

---
+++ NetCoreImportantLibraries/NetCoreLibrary.Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreLibrary.Core.Domain;
+
+namespace NetCoreLibrary.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ NetCoreImportantLibraries/NetCoreLibrary.Web/Containers/MicrosoftIocExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreLibrary.Data;
+
+namespace NetCoreLibrary.Web.Containers
+{
+    public static class MicrosoftIocExtension
+    {
+        public static void AddDbConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Default"));
+            });
+        }
+    }
+}
