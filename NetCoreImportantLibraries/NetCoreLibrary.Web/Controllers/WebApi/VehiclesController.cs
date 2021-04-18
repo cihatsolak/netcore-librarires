e053b7995da9acad470b9aba19c832863c6caa07
Cihat Solak <cihatsolak@hotmail.com>
2021-04-18 20:37:50 +0300

    Rate limit example and appsetting configuration

---
+++ NetCoreImportantLibraries/NetCoreLibrary.Web/Controllers/WebApi/VehiclesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreLibrary.Web.Controllers.WebApi
+{
+    public class VehiclesController : BaseApiController
+    {
+        /// <summary>
+        /// Rate limit kontrolü için
+        /// </summary>
+        [HttpGet]
+        public IActionResult RateLimitControl()
+        {
+            return Ok("No Problem!");
+        }
+    }
+}
