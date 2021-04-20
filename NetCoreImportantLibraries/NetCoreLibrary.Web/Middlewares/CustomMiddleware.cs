9e1c0bfe769786d0e2982a876ddbecadc4529c08
Cihat Solak <cihatsolak@hotmail.com>
2021-04-21 00:14:55 +0300

    Customize debug mode

--- NetCoreImportantLibraries/NetCoreLibrary.Web/Middlewares/CustomMiddleware.cs
+++ NetCoreImportantLibraries/NetCoreLibrary.Web/Middlewares/CustomMiddleware.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Smidge;
+using Smidge.Cache;
+using Smidge.Options;
 
 namespace NetCoreLibrary.Web.Middlewares
 {
@@ -10,8 +12,21 @@ namespace NetCoreLibrary.Web.Middlewares
             //Hangi dosyaları bundle etmek istiyorsam burada belirteceğim.
             app.UseSmidge(bundle =>
             {
+
                 bundle.CreateJs("my-js-bundle", "~/js/site.js", "~/js/site2.js");
+
                 bundle.CreateCss("my-css-bundle", "~/lib/bootstrap/dist/css/bootstrap.css", "~/css/site.css");
+                
+                
+                //bundle.CreateJs("my-js-bundle", "~/js/site.js", "~/js/site2.js")
+                ////ForDebug -> HTML tarafta debug="true" olan bundle dosyalarım için geçerli olacak kurallar. (Çok uç bir örnek, development ortamda da bundle etmek için yani var olan davranışı değiştirmek)
+                //.WithEnvironmentOptions(BundleEnvironmentOptions.Create().ForDebug(builder => builder.EnableCompositeProcessing() //debug="true" oldugunda da bundle(birleştir) et.
+                //                                                                                     .EnableFileWatcher() //bundle içerisindeki dosyalarda bir değişiklik olursa sıfırdan oluştur
+                //                                                                                     .SetCacheBusterType<AppDomainLifetimeCacheBuster>() //Uygulama her ayağa kalktığında cache sıfırlanacak
+                //                                                                                      //enableEtag:veriyi her seferinde cache'den al || cacheControlMaxAge:0 -> gelen datayı memoryde yada fiziksel diskde asla tutma
+                //                                                                                     .CacheControlOptions(enableEtag: false, cacheControlMaxAge: 0)) 
+                //                                                                                     .Build());
+
 
                 //eğer aynı klasör içerisinde birden fazla bundle edilmesi gereken dosya varsa, sadece dosya yolunu vermemiz yeterlidir.
                 //bundle.CreateJs("my-js-bundle", "~/js/");
