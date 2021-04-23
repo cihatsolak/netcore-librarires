2598f8cbfaf742044107c48be118074d27b4cc12
Cihat Solak <cihatsolak@hotmail.com>
2021-04-23 06:30:05 +0300

    SignUp example with fire and forget jobs

---
+++ NetCoreImportantLibraries/Hangfire.Server/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire.Server.Shedules;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hangfire.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SignUp()
+        {
+            FireAndForgotJobs.EmailSendToUserJob(userId: "123", message: "Hoşgeldin");
+            return Ok();
+        }
+    }
+}
